d3179c956e9cf72d4c9de79bee6b49f41fa1324d
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-18 14:47:40 +0100

    init generati 5 numeri casuali di simon

---
+++ js/script.js
@@ -0,0 +1,31 @@
+console.log('JS OK');
+
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta,
+// i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri,
+// il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+function randomNumberGenerator(min, max) {
+    const range = (max - min) + 1;
+    return Math.floor(Math.random() * range) + min;
+}
+
+function simonNumberGenerator(min, max) {
+    for (let i = 0; i < 5; i++) {
+        let simonNumber = randomNumberGenerator(min, max);
+        simonArray.push(simonNumber);
+        simonRandom.innerText = simonArray;
+    }
+
+
+}
+
+let simonArray = [];
+
+const simonRandom = document.getElementById('simonRandom');
+
+simonNumberGenerator(1, 100);
+
+console.log(simonArray);
+
